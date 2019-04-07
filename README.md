79c8ad43ba1dc6364c8a20eca2c0c659c27e0f57
Jake Bowden <Jake@jakes-mbp.home>
2019-04-07 13:03:00 +0100

    added main script, updated README

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Pomodo
+
+Minimal Pomodoro timer in python, see: https://en.wikipedia.org/wiki/Pomodoro_Technique
+
+Commands:
+> P/p - Pomodoro (25 minutes)
+> S/s - Short break (5 minutes)
+> L/l - Long break (10 minutes)
---
+++ pomodo.py
@@ -0,0 +1,26 @@
+# Pomodoro timer - Jake Bowden 04/19
+import time
+P = 0
+choices = ["p", "s", "l"]
+mode = ""
+while 1 > 0:
+    print('Pomodoros this session:', P)
+    # Collect timer setting:
+    while mode not in choices:
+        mode = str.lower(input('(P)omodoro/(S)hort/(L)ong?:'))
+    # Set time:
+    if mode == "p":
+        t = 25
+        P += 1
+    elif mode == "s":
+        t = 5
+    elif mode == "l":
+        t = 10
+    # Reset and run timer:
+    mins = 0
+    while mins != t:
+        print('>>>>', mins)
+        time.sleep(60)
+        mins += 1
+    # Play sound:
+    print("\a")

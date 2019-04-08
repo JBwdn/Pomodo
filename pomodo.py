145ab80f4d01b07e8fa1dd64400960af022bcbbb
J.Bowden <jake.bowden95@gmail.com>
2019-04-08 21:15:10 +0100

    added quit, reversed counter

--- pomodo.py
+++ pomodo.py
@@ -1,13 +1,13 @@
 # Pomodoro timer - Jake Bowden 04/19
 import time
 P = 0
-choices = ["p", "s", "l"]
-mode = ""
+choices = ["p", "s", "l", "q"]
 while 1 > 0:
+    mode = ""
     print('Pomodoros this session:', P)
     # Collect timer setting:
     while mode not in choices:
-        mode = str.lower(input('(P)omodoro/(S)hort/(L)ong?:'))
+        mode = str.lower(input('(P)omodoro/(S)hort/(L)ong/(Q)uit?:'))
     # Set time:
     if mode == "p":
         t = 25
@@ -16,12 +16,14 @@ while 1 > 0:
         t = 5
     elif mode == "l":
         t = 10
+    elif mode == "q":
+        break
     # Reset and run timer:
     mins = 0
+    print("Minutes Remaining:", end=" ", flush=True)
     while mins != t:
-        print(mins + 1, end=" ", flush=True)
+        print(t - mins, end=" ", flush=True)
         time.sleep(60)
         mins += 1
     # Play sound:
     print("\a")
-    mode = ""
